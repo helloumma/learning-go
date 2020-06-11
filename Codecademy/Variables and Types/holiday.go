36e4734352c12c23ca28c9f7e1e931e5042af60b
u-gohil <756D6D61@gmail.com>
2020-06-11 13:55:43 +0100

    More exercises

---
+++ Codecademy/Variables and Types/holiday.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	daysOnVacation:= 5
+	var hoursInDay = 24
+	fmt.Println("You have spent", daysOnVacation * hoursInDay, "hours on vacation.")
+}
---
+++ Codecademy/Variables and Types/magic.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main () {
+	var magicNum, powerLevel int32
+	magicNum = 2048
+	powerLevel = 9001
+	fmt.Println("magicNum is:", magicNum, "powerLevel is:", powerLevel)
+	
+	amount, unit := 10, "doll hairs"
+	fmt.Println(amount, unit, ", that's expensive...")
+}
